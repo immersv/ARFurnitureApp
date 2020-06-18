532ecdb6cc17356625b3d2d262d0cbb112b48252
immersvin <immersv@gmail.com>
2020-06-18 07:29:23 +0530

    Spawning object on horizontal plane

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class AppManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spawnObj;
+    [SerializeField] private Camera arCamera;
+    [SerializeField] private ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
+            if (raycastManager.Raycast(ray, hitResults))
+            {
+                Pose pose = hitResults[0].pose;
+                Instantiate(spawnObj, pose.position, pose.rotation);
+
+            }
+        }
+    }
+}
